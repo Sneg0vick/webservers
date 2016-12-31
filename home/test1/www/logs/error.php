58ff6a1759afd6f4800d3157ad19561515999cc8
Sneg0vick <infobiz@front.ru>
2016-12-31 23:28:26 +0300

    все

---
+++ home/test1/www/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-12-28 15:34:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-12-28T15:34:18+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
